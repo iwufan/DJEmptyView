19941369d33723dffddbe5f2a01b57a3bdeca937
贾大伟 <jiadawei@beadwallet.com>
2019-09-26 15:42:21 +0800

    Rewrite hit test method for empty view.

--- DJEmptyViewDemo/DJEmptyViewDemo/DJEmptyView/DJEmptyView.swift
+++ DJEmptyViewDemo/DJEmptyViewDemo/DJEmptyView/DJEmptyView.swift
@@ -68,6 +68,16 @@ public class DJEmptyView: UIView {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    /// You can hit the components below the empty view
+    public override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let hitView = super.hitTest(point, with: event)
+        
+        if hitView == self {
+            return nil
+        }
+        
+        return hitView
+    }
 }
 
 extension DJEmptyView {
